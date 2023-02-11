f79b6d61307b49d7cec4cdded8a7d889d6ca102b
Davide9728 <117565421+Davide9728@users.noreply.github.com>
2023-02-11 11:52:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 import data
 from data import resources, menu
-# TODO : 1) print report   \/
-# TODO : 2) check resources sufficient ? \/
-# TODO : 3) process coins \/
-# TODO : 4) check transaction successful? \/
-# TODO : 5) make a coffee \/
+# TODO : 1) print report   
+# TODO : 2) check resources sufficient ? 
+# TODO : 3) process coins 
+# TODO : 4) check transaction successful? 
+# TODO : 5) make a coffee 
 
 
-def print_report(): #stampa tutte le risorse del distributore, compresa la cassa
+def print_report(): #print all resource and money
     print("risorse rimaste")
     show_resource()
-    print(f"Money: {data.cassa}") #i soldi che abbiamo accumolato dalla vendita di bevande
+    print(f"Money: {data.cassa}")
     print()
 
-def show_resource(): #biene utilizzato da report per stampare gli ingredienti rimasti nel distributore
+def show_resource(): #used by report to print remaing resource
     for ingredient in resources:
         print(f"{ingredient}: {resources[ingredient]}")
 
-def check_resource_sufficient(chosen_drink):#controlla se è rimasta quantita di prodotto dopo il prelievo
+def check_resource_sufficient(chosen_drink):#cehck if there're anough resources to make chosen drink
 
     for remaining in menu[chosen_drink]["ingredients"]:
         if resources[remaining] - menu[chosen_drink]['ingredients'][remaining] <= 0:
@@ -25,25 +25,24 @@ def check_resource_sufficient(chosen_drink):#controlla se è rimasta quantita di
 
     return True
 
-def refresh_resources(chosen_drink):# aggiorna le quantità degli ingredienti  ---->  resource_in_stock[drink]['ingredients'][ingrediente]
+def refresh_resources(chosen_drink):# Update ingredient quantities ---->  resource_in_stock[drink]['ingredients'][ingrediente]
 
-     #accede alle quantità delle bevane in menu e aggiorna lo stock di ingredienti
     for ingredient in menu[chosen_drink]["ingredients"]:
-        the_resource = menu[chosen_drink]['ingredients'][ingredient]  #accede alle quantita dell'ingrediente della bevanda scelta
-        # aggiorna lo stock di ingredienti
+        the_resource = menu[chosen_drink]['ingredients'][ingredient]  #it get ingredient quantities of choosen drink
+        # update of ingredient stock
         resources[ingredient] -= the_resource
 
-def process_coin(chosen_drink):
-    price = menu[chosen_drink]["cost"]
+def process_coin(chosen_drink):#ceck if are enough money
+    price = menu[chosen_drink]["cost"]#it get price of chosen drink
     print(f"il costo della bevanda: {price}")
-    money = check_transaction_succesful(price) #qui controlla se i soldi inseriti sono sufficenti
+    money = check_transaction_succesful(price)
     if money >= price:
         data.cassa += price
         return money - price
     else:
         return "Sorry that's not enough money"
 
-def check_transaction_succesful(price):
+def check_transaction_succesful(price): #ask money  to pay chosen drink
     PENNY = 0.01
     NIKEL = 0.05
     DIME = 0.10
@@ -58,19 +57,19 @@ def check_transaction_succesful(price):
 #start
 on_off = False
 while on_off != True:
-    choose = input("what would you like? (espresso/latte/cappuccino):")
-    if choose == "report":
+    chosen = input("what would you like? (espresso/latte/cappuccino):")
+    if chosen == "report":
         print_report()
-    elif choose == "off":
+    elif chosen == "off":
         on_off = True
     else:
-        rest = process_coin(choose)
+        rest = process_coin(chosen)
         if type(rest) != str:
             print(f"the rest: {rest}")
 
-            if check_resource_sufficient(choose):
-                refresh_resources(choose)
-                print(f"enjoy your {choose}")
+            if check_resource_sufficient(chosen):
+                refresh_resources(chosen)
+                print(f"enjoy your {chosen}")
             else:
                 print("resources not found")
                 on_off = True
